9678c320bd44b00b330db879aeee201f7da1706a
Your Name <you@example.com>
2022-11-11 15:58:20 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMayName';
-import Pessoa from './components/Pessoa';
 import Inicio from './components/Inicio';
 
 
 function App() {
-  // const nome = "kevin"
   return (
     <div>
       <Inicio />
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -7,10 +7,22 @@ function Inicio() {
         <div className={styles.content}>
             <nav className={styles.navTop}>
                 <ul className={styles.ulNav}>
-                    <h1 onMouseOver={Swal.fire('Any fool can use a computer')} className={styles.h1Nav}>TESTES</h1>
-                    <li className={styles.liNav}>
-                        Duvidas
+                <li className={styles.liNav}>
+                        Portifólio
                     </li>
+                    <li className={styles.liNav}>
+                        Experiências
+                    </li>
+                    <h1 className={styles.h1Nav}>Kezinit</h1>
+                    {/* <h2 className={styles.h2Nav}>Programador de Designer</h2> */}
+                    <div className={styles.divli}>
+                    <li className={styles.liNav}>
+                        Sobre Mim
+                    </li>
+                    <li className={styles.liNav}>
+                        Contato
+                    </li>
+                    </div>
                 </ul>
             </nav>
             <div>
